78643d5752d1eaaa47e4e4bd8d90a33aaa9c8042
QuangDo <qdangdo@gmail.com>
2021-10-26 09:16:38 -0500

    Install bcrpyt for password authetication

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
     has_many :categories, through: :spendings
     has_many :months, through: :spendings
 
+    # Bcrypt function that saves the password safely
+    # INSTANCE.authenticate(password) to check if its true
+    has_secure_password 
+
 end
