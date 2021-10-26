63e0b79a3677affd48f52472098e7602817f818b
QuangDo <qdangdo@gmail.com>
2021-10-26 18:08:39 -0500

    Reformat code in controller to models

--- app/controllers/spending_controller.rb
+++ app/controllers/spending_controller.rb
@@ -3,18 +3,7 @@ class SpendingController < ApplicationController
 
     get '/spending/:user_id' do
         user = User.find(params[:user_id]) #Takes in id number to find user
-        user_specific_spendings = Spending.where(user_id: params[:user_id])
-        data = user_specific_spendings.map do |spending|
-            {
-                id: spending.id,
-                description: spending.description,
-                amount: spending.amount,
-                user_id: spending.user_id,
-                month_id: spending.month_id,
-                category: spending.category.description,
-                date: spending.date
-            }
-        end
+        data = user.renders_user_spendings
         data.to_json
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,17 @@ class User < ActiveRecord::Base
     # INSTANCE.authenticate(password) to check if its true
     has_secure_password 
 
+    def renders_user_spendings
+        user_specific_spendings = Spending.where(user_id: self.id)
+        data = user_specific_spendings.map do |spending|
+            {
+                description: spending.description,
+                amount: spending.amount,
+                id: spending.id,
+                date: spending.date,
+                category: spending.category.description
+            }
+        end
+    end
+
 end
