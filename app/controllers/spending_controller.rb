00d728f3a1af0bdd02f615eb2d24bf488caab53c
Jon <jonpilarski927@gmail.com>
2021-10-27 12:58:47 -0500

    Update spending Controller

--- app/controllers/spending_controller.rb
+++ app/controllers/spending_controller.rb
@@ -7,4 +7,20 @@ class SpendingController < ApplicationController
         data.to_json
     end
 
+    post '/spending/:user_id' do
+        spending = Spending.create(description: params[:amount], amount: params[:amount], user: params[:user_id], month: params[:month_id], date: params[:date], category_id: params[:category_id] )
+        spending.to_json
+    end
+
+    patch '/spending/:id/edit'do
+        spending = Spending.find(params[:id])
+        spending.update(amount: params[:amount])
+        spneding.to_json
+    end
+
+    delete '/spending/:id' do
+        spending = Spending.find(params[:id])
+        spending.destroy
+    end
+
 end
