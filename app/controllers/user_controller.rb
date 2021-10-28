2f221b42458b5e9700701ca214d81aa93684ce92
Jon <jonpilarski927@gmail.com>
2021-10-28 12:30:35 -0500

    Set up SignUp Post

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -9,7 +9,15 @@ class UserController < ApplicationController
         else
             return nil.to_json
         end
-        # user.autheticate(params[:password])
     end
 
+    post "/signup" do
+        if (User.find_by(email: params[:email]))
+            return nil.to_json
+        else
+            user = User.create(full_name: params[:full_name], email: params[:email], password_digest: params[:password_digest])
+            budgetPlan = Budget_plan.create(user_id: params[:used_id], housing_percentage: 0.125, transport_percentage: 0.125, food_percentage: 0.125, utility_percentage: 0.125, insurance_percentage: 0.125, emergency_percentage: 0.125, discretionary_percentage: 0.125, other_percentage: 0.125, balance: 1000)
+            return user.to_json
+        end
+    end
 end
